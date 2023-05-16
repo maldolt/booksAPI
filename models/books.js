2630daf06d1c46f96e143cfef2be8cd013204735
Hadeel Ammari <80487488+hadeelammari@users.noreply.github.com>
2023-05-16 13:08:38 -0700

    Update books.js

--- models/books.js
+++ models/books.js
@@ -4,15 +4,13 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 // schema
-const booksSchema = new Schema({
-  id: 1,
-  title: "The Shinobi Initiative",
-  description:
-    "The reality-bending adventures of a clandestine service agency in the year 2166",
-  year: 2014,
-  quantity: "10",
-  imageURL: "/assets/shinobi-initiative.jpeg",
-});
+const bookSchema = new Schema({
+  title: String,
+  description: String,
+  year: Number,
+  quantity: Number,
+  imageURL: String
+})
 
   
-module.exports = mongoose.model('Book', booksSchema)
+module.exports = mongoose.model('Book', bookSchema)
